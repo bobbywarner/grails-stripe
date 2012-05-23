5150c65754e71de61ba26ce89b8a9f7b855dae2d
Bobby Warner <bobbywarner@gmail.com>
2012-05-22 20:44:31 -0500

    updates for 1.1 release

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,12 +16,12 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        compile 'com.stripe:stripe-java:1.0.12'
+        compile 'com.stripe:stripe-java:1.1.0'
     }
 
     plugins {
         compile ":resources:1.1.6"
-        build(":release:1.0.1") {
+        build(":release:2.0.2") {
             export = false
         }
     }
