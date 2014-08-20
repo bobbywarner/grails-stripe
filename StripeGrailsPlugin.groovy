8f3b0e4ee31f9c04b706c39e1515bb023ceea095
Bobby Warner <bobbywarner@gmail.com>
2014-08-20 11:59:51 -0500

    Bump version to 2.8

--- StripeGrailsPlugin.groovy
+++ StripeGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 class StripeGrailsPlugin {
-    def version = "2.7"
+    def version = "2.8"
     def grailsVersion = "1.3.7 > *"
     def loadAfter = ['resources', 'jquery']
 
